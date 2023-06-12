528ac6b674c5d70f79ab71c0494e4b197b5b4b20
Luis Martínez <lemartinezmendoza@gmail.com>
2023-06-12 17:31:50 -0500

    config: add connection with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,25 @@ import { AlbumsModule } from './albums/albums.module';
 import { SongsModule } from './songs/songs.module';
 import { EventsModule } from './events/events.module';
 import { LyricsModule } from './lyrics/lyrics.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [InfoModule, SongsModule, AlbumsModule, EventsModule, LyricsModule],
+  imports: [
+    InfoModule,
+    SongsModule,
+    AlbumsModule,
+    EventsModule,
+    LyricsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

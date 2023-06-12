141bdbf71c8846345b3c5f96aead0c68f973053a
Luis Martínez <lemartinezmendoza@gmail.com>
2023-06-12 16:56:01 -0500

    feat: add project structure and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InfoModule } from './info/info.module';
+import { AlbumsModule } from './albums/albums.module';
+import { SongsModule } from './songs/songs.module';
+import { EventsModule } from './events/events.module';
+import { LyricsModule } from './lyrics/lyrics.module';
 
 @Module({
-  imports: [],
+  imports: [InfoModule, SongsModule, AlbumsModule, EventsModule, LyricsModule],
   controllers: [AppController],
   providers: [AppService],
 })

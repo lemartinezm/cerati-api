e9ded5acaf185481c8e5293fdf681bf229987376
Luis Martínez <lemartinezmendoza@gmail.com>
2023-09-05 16:11:01 -0500

    fix: delete events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { SongsModule } from './songs/songs.module';
-import { EventsModule } from './events/events.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -11,7 +10,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     SongsModule,
     AlbumsModule,
-    EventsModule,
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
